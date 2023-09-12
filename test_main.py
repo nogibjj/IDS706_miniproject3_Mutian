07c62ae07b448019887f31ce69fa39eb3e1efb17
Liam <ml646@duke.edu>
2023-09-12 12:21:49 -0400

    update main

--- test_main.py
+++ test_main.py
@@ -1,12 +1,12 @@
 from main import *
 
 def test_func():
-    df = loadDf("https://people.sc.fsu.edu/~jburkardt/data/csv/oscar_age_female.csv")
+    df = loadDf("./taker_buy_sell_volume.csv")
     descriptive_df = describeData(df)
-    assert isinstance(df, pd.DataFrame)
-    assert 'max' in descriptive_df.index
-    assert df.shape[0]==89
+    assert isinstance(df, pl.DataFrame)
+    #assert 'max' in descriptive_df.index
+    #assert df.shape[0]==89
     print(descriptive_df)
     print(df)
-    plotData(df)
+    #plotData(df)
 test_func()
